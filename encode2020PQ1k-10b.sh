8e33841c3700670b2c7cda29b76aecf0d05e5a7f
Quantization Bit <quantizationbit@users.noreply.github.com>
2014-06-28 17:13:24 -0700

    added repeat headers

--- encode2020PQ1k-10b.sh
+++ encode2020PQ1k-10b.sh
@@ -1,7 +1,7 @@
 set -x
 
 
-x265 --input 2020PQ1k-10b.yuv --input-depth 10 --input-res 1920x1080 --fps 23.976 --crf 23 --vbv-maxrate 20000 --vbv-bufsize 70000  -p medium --bframes 12 -I 72 --psnr --sar 1 --range limited --colorprim bt2020 --transfer bt2020-10 --colormatrix bt2020nc --chromaloc 1 --no-lft -o 2020PQ1k-10b.bin 2>&1 | tee log-2020PQ1k-10b.txt
+x265 --input 2020PQ1k-10b.yuv --input-depth 10 --input-res 1920x1080 --fps 23.976 --crf 23 --vbv-maxrate 20000 --vbv-bufsize 70000  -p medium --bframes 12 -I 72 --psnr --sar 1 --range limited --colorprim bt2020 --transfer bt2020-10 --colormatrix bt2020nc --chromaloc 1 --no-lft --repeat-headers -o 2020PQ1k-10b.bin 2>&1 | tee log-2020PQ1k-10b.txt
 
 
 
